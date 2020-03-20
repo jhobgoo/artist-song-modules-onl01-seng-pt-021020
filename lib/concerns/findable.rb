15e57cd5272f7a6e57b343b2f68154773ef30e11
Joel Hobgood <jhobgoo@gmail.com>
2020-03-20 15:13:15 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,6 +1,17 @@
 module Findable
+  
   module ClassMethods
     
     def find_by_name(name)
       @@artists.detect{|a| a.name == name}
     end
+    
+  end
+  
+  module InstanceMethods
+    
+    def all
+      @@all << self
+    end
+
+end
