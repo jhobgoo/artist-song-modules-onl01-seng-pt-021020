7b4a0e4edc1469271b4e4ac1392c90ae454eff2d
Joel Hobgood <jhobgoo@gmail.com>
2020-03-20 20:07:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,7 +22,7 @@ class Song
 
   def artist=(artist)
     @artist = artist
-    artist.add_song(self) unless artist.songs(self)include?(self)
+    artist.add_song(self) unless artist.songs.include?(self)
   end
   
 end
